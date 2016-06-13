61f7164cb54fe7983966cd97b5ef2683cda394a2
TheZoq2 <frans.skarman@gmail.com>
2016-06-13 10:57:15 +0200

    initial commit

---
+++ src/rc.rs
@@ -0,0 +1,28 @@
+//Unit size nut
+fn base_nut() -> ScadObject
+{
+    let cube_len = 1.0/2.0;
+    let cube_width = 30.0_f32.to_radians().sin() / 30.0_f32.to_radians().cos();
+
+    let mut result = scad!(Union);
+
+    for i in 0..6
+    {
+        result.add_child(
+            scad!(Rotate(60.0 * i as f32 + 30.0, vec3(0.0, 0.0, 1.0));
+            {
+                scad!(Translate(vec3(-cube_width / 2.0, 0.0, 0.0)); scad!(Cube(vec3(cube_width, cube_len, 1.0))))
+            })
+        );
+    }
+
+    result
+}
+
+fn nut(width: f32, height: f32) -> ScadObject
+{
+    scad!(Scale(vec3(width, width, height));
+    {
+        base_nut()
+    })
+}
