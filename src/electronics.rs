0a44673168a4602802cbb8d98e6014beb19834ad
TheZoq2 <frans.skarman@gmail.com>
2016-10-10 19:01:07 +0200

    Make teensy USB port slightly bigger

--- src/electronics.rs
+++ src/electronics.rs
@@ -6,10 +6,11 @@ pub fn teensy_lc() -> ScadObject
     let length = 36.0;
     let height = 2.0;
 
-    let usb_width = 8.;
+    let usb_width = 8.5;
     let usb_height = 4.;
     let usb_length = 6.;
     let usb_offset = 2.;
+    let usb_bottom_offset = 0.5;
 
 
     let pcb = 
@@ -24,7 +25,7 @@ pub fn teensy_lc() -> ScadObject
         let cube = scad!(Cube(vec3(usb_width, usb_length, usb_height)));
         
         let centered = scad!(Translate(vec3(-usb_width / 2., 0., 0.)); cube);
-        scad!(Translate(vec3(0., -usb_offset, height)); centered)
+        scad!(Translate(vec3(0., -usb_offset, height - usb_bottom_offset)); centered)
     };
 
     scad!(Union;
