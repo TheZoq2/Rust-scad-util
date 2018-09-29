2ac1bd90e4d52d4437abf01fba378f6b3e1aacb0
TheZoq2 <frans.skarman@gmail.com>
2018-09-29 10:19:49 +0200

    Add keyboard module

---
+++ src/keyboard.rs
@@ -0,0 +1,16 @@
+use scad::*;
+
+pub fn mx_switch_hole() -> ScadObject 
+{
+    let inner_height = 1.5;
+    let padding = 0.2;
+    let inner_width = 14. + padding * 2.;
+
+
+    let cube = scad!(Cube(vec3(inner_width, inner_width, inner_height)));
+
+    scad!(Translate(vec3(-inner_width / 2., -inner_width / 2., 0.));
+    {
+        cube
+    })
+}
